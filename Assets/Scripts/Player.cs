eb90e93d6ed2326744735c620cd3981f5fcc256c
caruso-felipe <caruso.f@usal.edu.ar>
2026-06-02 12:53:35 -0300

    Animacion Idle
    Ya se configuro la animacion de idle, y se conecto con el movimiento

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,40 +11,49 @@ public class Player : MonoBehaviour
     public bool enSigilo = false;
 
     private Rigidbody2D rb;
-    private Vector2 movimiento;
-    private float velocidadActual;
-
-    // Conectar SpriteRenderer
     private SpriteRenderer sr;
 
     // Animaciones
-    private float movimientoX;
-    private float movimientoY;
     private Animator animator;
 
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
+    private Vector2 movimiento;
+    private float velocidadActual;
+
+
+   
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        animator.SetFloat("UltimoX", 0f);
+        animator.SetFloat("UltimoY", -1f);
     }
 
     void Update()
     {
-        // Animacion eje Horizontal
-        movimientoX = Input.GetAxisRaw("Horizontal");
-        movimientoY = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("MovimientoX", movimientoX);
-        animator.SetFloat("MovimientoY", movimientoY);
+        // Animacion eje Horizontal Y Vertical
+        movimiento.x = Input.GetAxisRaw("Horizontal");
+        movimiento.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("MovimientoX", movimiento.x);
+        animator.SetFloat("MovimientoY", movimiento.y);
 
-        // Input de movimiento (WASD o flechas) 
-        movimiento.x = Input.GetAxisRaw("Horizontal"); // A/D o flechas
-        movimiento.y = Input.GetAxisRaw("Vertical");   // W/S o flechas
+        animator.SetBool("Caminando", movimiento.x != 0 || movimiento.y != 0);
 
+        // Animacion Idle
+        if (movimiento.x != 0 || movimiento.y != 0)
+        {
+            animator.SetFloat("UltimoX", movimiento.x);
+            animator.SetFloat("UltimoY", movimiento.y);
+        }
+
+        // Input de movimiento (WASD o flechas)
         // Normalizar para evitar movimiento diagonal más rápido
         if (movimiento.magnitude > 1f)
             movimiento.Normalize();
@@ -60,7 +69,7 @@ public class Player : MonoBehaviour
                 sr.color = enSigilo ? new Color(0.5f, 0.7f, 1f, 0.75f) : Color.white;
         }
 
-        // Prioridad de velocidad 
+        // Prioridad de velocidad
         // Sigilo tiene prioridad sobre sprint (no tiene sentido correr en sigilo)
         if (enSigilo)
         {
